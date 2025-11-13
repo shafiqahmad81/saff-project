273fd952a1748622fe8fcefedbe319bc2eeb30f0
shafiqahmad81 <shafiqahmad081@gmail.com>
2025-11-13 21:58:58 +0600

    update complete project

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 export default defineConfig({
   plugins: [
     tailwindcss(),
   ],
+    build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'index.html'),
+            travel: resolve(__dirname, 'travel-claim.html'),
+          },
+        },
+      },
 })
